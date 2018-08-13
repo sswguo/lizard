c7ccc257a1bba2e45ec5194ecd279a9c50c7c132
Wenjie Guo <wguo@redhat.com>
2018-08-13 17:17:47 +0800

    add pagination

--- lizard-server/lizard-server-web/src/main/webapp/js/examples/dataTable.js
+++ lizard-server/lizard-server-web/src/main/webapp/js/examples/dataTable.js
@@ -30,7 +30,7 @@ class DataTableComponent extends React.Component{
       return <div>Error: {error.message}</div>;
     } else {
       return (
-	    <BootstrapTable data= {items} search headerStyle={ { background: '#e0ffe2' } }>
+	    <BootstrapTable data= {items} search headerStyle={ { background: '#e0ffe2' } } pagination>
            <TableHeaderColumn isKey dataField='id'>Task ID</TableHeaderColumn>
            <TableHeaderColumn dataField='name'>Task Name</TableHeaderColumn>
            <TableHeaderColumn dataField='description'>Description</TableHeaderColumn>
